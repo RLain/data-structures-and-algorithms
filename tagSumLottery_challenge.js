973841477b7ecbfdc7814ad8ebed8d8e53356f9a
Rebecca Lain <aeroboticsadmin@MacBook-Pro.lan>
2026-03-17 09:09:04 +0200

    map challenge

--- tagSumLottery_challenge.js
+++ tagSumLottery_challenge.js
@@ -57,6 +57,7 @@ Notes:
 */
 
 // The original algorithm:
+// Your job is to improve the time complexity to something better than O(n^2).
 export function tagSumLottery(communications, lotteryNumber) {
   const winners = [];
   for (let i = 0; i < communications.length; i++) {
@@ -75,3 +76,61 @@ export function tagSumLottery(communications, lotteryNumber) {
 }
 
 // The updated algorithm:
+
+export function tagSumLottery(communications, lotteryNumber) {
+  if (!communications || !communications.length) return 'No winners (No comms)';
+
+  const winners = [];
+  const tagNames = new Map(); //Maps have a lookup of O(1)
+
+  for (const { tag, name } of communications) {
+    const result = lotteryNumber - tag;
+    if (tagNames.has(result)) {
+      const match = tagNames.get(result);
+      winners.push([name, match].toSorted());
+    } else {
+      tagNames.set(tag, name);
+    }
+  }
+
+  if (!winners.length) return 'No winners';
+  return winners;
+}
+
+// Instructors solution:
+/*
+Algorithm: Tag Sum Lottery
+Input: array of objects, each has a tag and name, and a lottery number
+Output: array of pairs of names, whose tags add up to the lottery number. Each pair is sorted alphabetically. If there are no winners, return the string "No winners".
+
+Steps:
+1. Init a winners array.
+2. Init a tagName map.
+3. Loop over the array:
+4.   If the difference between the lottery number and the current tag is in the map:
+5.     Add both the name the tag found in the map maps to, and the current name, as an array, sorted, to the winners array.
+6.   Else:
+7.     Add the current tag and name to the map.
+8. If we found pairs of winners:
+9.   Return them.
+10. Return "No winners".  
+*/
+
+export function tagSumLottery(communications, lotteryNumber) {
+  const winners = [];
+  const tagName = new Map();
+  for (let i = 0; i < communications.length; i++) {
+    const difference = lotteryNumber - communications[i].tag;
+    if (tagName.has(difference)) {
+      winners.push(
+        [tagName.get(difference), communications[i].name].toSorted()
+      );
+    } else {
+      tagName.set(communications[i].tag, communications[i].name);
+    }
+  }
+  if (winners.length > 0) {
+    return winners;
+  }
+  return 'No winners';
+}
